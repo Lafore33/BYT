cdaedb8b11652f73132fa7399ae3095c2e664927
Mikhail Koutun <mkov2n1517@gmail.com>
2025-11-09 17:33:17 +0100

    Initial commit

---
+++ src/main/java/com/example/byt/BytApplication.java
@@ -0,0 +1,13 @@
+package com.example.byt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/byt/BytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.byt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
