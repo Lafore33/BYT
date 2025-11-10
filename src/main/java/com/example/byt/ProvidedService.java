bdeba36788928de30085df5c9035b8823b380d7d
Marharyta21 <s29873@pjwstk.edu.pl>
2025-11-10 16:53:13 +0100

    Material, ProvidedService, TwoHandsService, FourHandsService classes

---
+++ src/main/java/com/example/byt/ProvidedService.java
@@ -0,0 +1,15 @@
+package com.example.byt;
+
+public class ProvidedService {
+    private double rating;
+    private String comment;
+    private double time;
+    private double price;
+
+    public ProvidedService(double rating, String comment, double time, double price) {
+        this.rating = rating;
+        this.comment = comment;
+        this.time = time;
+        this.price = price;
+    }
+}
