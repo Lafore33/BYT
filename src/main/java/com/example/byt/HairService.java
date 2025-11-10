bccde1b52143dbcf2104d9df19c24371945d4127
nazarchukdana <nazarchuk.dana05@gmail.com>
2025-11-10 15:17:56 +0100

    fix: hairservice extends service

--- src/main/java/com/example/byt/HairService.java
+++ src/main/java/com/example/byt/HairService.java
@@ -2,11 +2,13 @@ package com.example.byt;
 
 import java.util.List;
 
-public class HairService {
+public class HairService extends Service{
     private HairServiceType type;
     private List<String> hairTypes;
 
-    public HairService(HairServiceType type, List<String> hairTypes) {
+    public HairService(int id, String name, double regularPrice, String description, double duration, double rating,
+                       double totalPrice, HairServiceType type, List<String> hairTypes) {
+        super(id, name, regularPrice, description, duration, rating, totalPrice);
         this.type = type;
         this.hairTypes = hairTypes;
     }
