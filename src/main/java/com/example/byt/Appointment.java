33efe6383122356f6260ad2c0797a2840d961a42
liz <s30187@pjwstk.edu.pl>
2025-11-12 17:21:04 +0100

    Added validations for Appointment,Certification,HistoryOfStatus,Promotion

--- src/main/java/com/example/byt/Appointment.java
+++ src/main/java/com/example/byt/Appointment.java
@@ -1,14 +1,26 @@
 package com.example.byt;
 
-import java.util.Date;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class Appointment {
-    private Date date;
-    private String notes;
+    // as the appointment might be completed, so the date will be in the past
+    // but it can be scheduled as well, that mean it is in the future
+    @NotNull
+    private LocalDate date;
+
+    private List<String> notes;
+
+    @NotNull
     private PaymentMethod paymentMethod;
+
+    @Min(0)
     private double totalPrice;
 
-    public Appointment(Date date, String notes, PaymentMethod paymentMethod, double totalPrice) {
+    public Appointment(LocalDate date, List<String> notes, PaymentMethod paymentMethod, double totalPrice) {
         this.date = date;
         this.notes = notes;
         this.paymentMethod = paymentMethod;
--- src/main/java/com/example/byt/Certification.java
+++ src/main/java/com/example/byt/Certification.java
@@ -1,22 +1,51 @@
 package com.example.byt;
 
-import java.util.Date;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
 
 public class Certification {
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String certificationNumber;
+
+    @NotBlank
     private String description;
+
+    @NotBlank
     private String organization;
-    private Date issueDate;
-    private Date expiryDate;
+
+    @PastOrPresent
+    @NotNull
+    private LocalDate issueDate;
+
+    private LocalDate expiryDate;
 
     public Certification(String name, String certificationNumber, String description,
-                         String organization, Date issueDate, Date expiryDate) {
+                         String organization, LocalDate issueDate, LocalDate expiryDate) {
         this.name = name;
         this.certificationNumber = certificationNumber;
         this.description = description;
         this.organization = organization;
         this.issueDate = issueDate;
+        setExpiryDate(expiryDate);
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        if (expiryDate != null && issueDate.isAfter(expiryDate)) {
+            throw new IllegalArgumentException("expiryDate must be after issueDate");
+        }
         this.expiryDate = expiryDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        if (issueDate.isAfter(expiryDate)) {
+            throw new IllegalArgumentException("expiryDate must be after issueDate");
+        }
+        this.issueDate = issueDate;
     }
 }
