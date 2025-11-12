9ad5fbf0bed459f6e772495064bcde76a68d4f1d
Marharyta21 <s29873@pjwstk.edu.pl>
2025-11-12 16:20:26 +0100

    Feature/ FourHandsService was added "final"

--- src/main/java/com/example/byt/FourHandsService.java
+++ src/main/java/com/example/byt/FourHandsService.java
@@ -2,7 +2,7 @@ package com.example.byt;
 
 public class FourHandsService extends Service{
     private boolean isExpressService;
-    private static int numOfSpecialistsRequired = 2;
+    private final static int numOfSpecialistsRequired = 2;
 
     public FourHandsService(int id, String name, double regularPrice, String description,
                             double duration, double rating, double totalPrice, boolean isExpressService) {
