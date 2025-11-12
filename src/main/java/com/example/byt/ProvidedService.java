589210e9afc6ff8d89d0bdfe6db8be14d2fcad93
Marharyta21 <s29873@pjwstk.edu.pl>
2025-11-12 16:36:52 +0100

    Feature/ Changed in ProvidedService from double to LocalDateTime

--- src/main/java/com/example/byt/ProvidedService.java
+++ src/main/java/com/example/byt/ProvidedService.java
@@ -1,17 +1,19 @@
 package com.example.byt;
 import jakarta.validation.constraints.*;
 
+import java.time.LocalDateTime;
+
 public class ProvidedService {
     @Min(0)
     @Max(5)
     private int rating;
     private String comment;
     @Min(0)
-    private double time;
+    private LocalDateTime time;
     @Min(0)
     private double price;
 
-    public ProvidedService(int rating, String comment, double time, double price) {
+    public ProvidedService(int rating, String comment, LocalDateTime time, double price) {
         this.rating = rating;
         this.comment = comment;
         this.time = time;
