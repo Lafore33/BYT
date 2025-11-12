938719b0d5db800a2d4af0581b4bc1381fae9626
Marharyta21 <s29873@pjwstk.edu.pl>
2025-11-12 16:33:27 +0100

    Feature/ Deleted in TwoHandsService express

--- src/main/java/com/example/byt/TwoHandsService.java
+++ src/main/java/com/example/byt/TwoHandsService.java
@@ -1,12 +1,10 @@
 package com.example.byt;
 
 public class TwoHandsService extends Service {
-    private boolean isExpressService;
     private final static int numOfSpecialistsRequired = 1;
 
     public TwoHandsService(int id, String name, double regularPrice, String description,
-                           double duration, double rating, double totalPrice, boolean isExpressService) {
+                           double duration, double rating, double totalPrice) {
         super(id, name, regularPrice, description, duration, rating, totalPrice);
-        this.isExpressService = isExpressService;
     }
 }
