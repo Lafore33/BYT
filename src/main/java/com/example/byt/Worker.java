56393d23111008a09ae1d4573bb96a21b0ab5356
Mikhail Koutun <mkov2n1517@gmail.com>
2025-11-12 16:55:09 +0100

    Added field validation for person related classes

--- src/main/java/com/example/byt/Worker.java
+++ src/main/java/com/example/byt/Worker.java
@@ -1,12 +1,29 @@
 package com.example.byt;
 
-import java.util.Date;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
 
 public class Worker extends Person {
-    private Date birthDate;
 
-    public Worker(String name, String surname, String phoneNumber, Date birthDate) {
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    public Worker(String name, String surname, String phoneNumber, LocalDate birthDate) {
         super(name, surname, phoneNumber);
+        setBirthDate(birthDate);
+    }
+
+    private boolean isBirthDateValid(LocalDate birthDate) {
+        return birthDate.plusYears(18).isBefore(LocalDate.now());
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        if (!isBirthDateValid(birthDate)) {
+            throw new IllegalArgumentException("The worker should be at least 18 years old");
+        }
         this.birthDate = birthDate;
     }
 }
