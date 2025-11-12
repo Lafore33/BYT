63f33cd2fd3ff53ad76d36fd3bdd8dc057e7a3c8
nazarchukdana <nazarchuk.dana05@gmail.com>
2025-11-12 16:17:09 +0100

    add validation annotations for attributes of base service class and service types

--- src/main/java/com/example/byt/SkinService.java
+++ src/main/java/com/example/byt/SkinService.java
@@ -1,7 +1,10 @@
 package com.example.byt;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class SkinService extends Service {
 
+    @NotBlank
     private String purpose;
 
     public SkinService(int id, String name, double regularPrice, String description,
