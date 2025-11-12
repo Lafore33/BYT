4eb2ef3f8f134122f6ef368bb7e1be27c662f148
Marharyta21 <s29873@pjwstk.edu.pl>
2025-11-12 16:20:42 +0100

    Feature/ TwoHandsService was added "final"

--- src/main/java/com/example/byt/TwoHandsService.java
+++ src/main/java/com/example/byt/TwoHandsService.java
@@ -1,10 +1,12 @@
 package com.example.byt;
 
 public class TwoHandsService extends Service {
-    private static int numOfSpecialistsRequired = 1;
+    private boolean isExpressService;
+    private final static int numOfSpecialistsRequired = 1;
 
     public TwoHandsService(int id, String name, double regularPrice, String description,
-                           double duration, double rating, double totalPrice) {
+                           double duration, double rating, double totalPrice, boolean isExpressService) {
         super(id, name, regularPrice, description, duration, rating, totalPrice);
+        this.isExpressService = isExpressService;
     }
 }
